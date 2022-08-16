047cb26384ba00f7c134fb6667a0587955ddf0f4
alexiom <alexander.marsh22@gmail.com>
2022-08-16 11:48:46 -0400

    Create Github Repo Dump

---
+++ R_Script_Template.R
@@ -0,0 +1,79 @@
+################################################################################
+# Title: Template R Script                                                     #
+# Author: Alex Marsh                                                           #
+# Date: March 14, 2022                                                         #
+# Notes:                                                                       #
+################################################################################
+
+# Note: I use the pound symbol (hashtags) across the entire line to make
+#       banner comment. I use these almost exclusively for the title banner 
+#       (the banner above), and the description banner (the banner below).
+
+
+# My style is heavily influenced by the following link:
+# https://cran.r-project.org/web/packages/bannerCommenter/vignettes/Banded_comment_maker.pdf
+# My banners deviate a little from what is shown in the linked pdf (I don't like
+# the bigger and blockier banners) but the ideas remain the same.
+
+################################################################################
+#                                Description:                                  #
+# Description of script (what it does, what it requires, what it creates,      #
+# input and output folders, etc.) goes here. You should be as thorough as      #
+# possible here.                                                               #
+################################################################################
+
+
+#==============================================================================#
+#                           Script Preparation                                 #
+#==============================================================================#
+
+# Note: I use the equals sign for the banners of "major sections." For "minor 
+#       sections," I use dashes. What is a major section vs minor section is a 
+#       judgement call, and you will figure this out with experience (I still 
+#       sometimes struggle with figuring out the difference). Minor sections
+#       should be nested within minor sections. 
+
+
+# What goes here? library() statements to load packages, setting directories,  #
+# global variables/constant that will NEVER change throughout the script, etc. #
+# Since the library section and section to set global variables and constants/ #
+# parameters are both part of the script preparation major section, they should#
+# be separated as minor sections.                                              #
+
+
+#------------------------------------------------------------------------------#
+#                              Library packages                                #
+#------------------------------------------------------------------------------#
+
+# Note: This is a minor section because of the dashed banner.
+
+#------------------------------------------------------------------------------#
+#                   Set Global Variables and Parameters                        #
+#------------------------------------------------------------------------------#
+
+
+#==============================================================================#
+# Every script you write from now on should have almost the same exact         #
+# structure as above. As such, you can use it as a template for the scripts you#
+# write from here on out. What will change is the code that comes next. That   #
+# will depend largely on what you are writing.                                 #
+#==============================================================================#
+
+
+#==============================================================================#
+#                            First Major Section                               #
+#==============================================================================#
+
+
+#------------------------------------------------------------------------------#
+#                            First Minor Section                               #
+#------------------------------------------------------------------------------#
+
+
+
+
+
+
+################################################################################
+#                                END OF SCRIPT                                 #
+################################################################################
